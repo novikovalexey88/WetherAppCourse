c1197bca1d097050045facf1b490cca4801b6e0f
novikovalexey88 <novikov.a.m@yandex.ru>
2022-11-17 23:34:56 +0300

    permission

---
+++ app/src/main/java/com/example/wetherappcourse/DayItem.kt
@@ -0,0 +1,12 @@
+package com.example.wetherappcourse
+
+data class DayItem(
+    val city: String,
+    val time: String,
+    val condition: String,
+    val imageUrl: String,
+    val currentTemp: String,
+    val maxTemp: String,
+    val mintTemp: String,
+    val hours: String
+)
--- app/src/main/java/com/example/wetherappcourse/fragments/MainFragment.kt
+++ app/src/main/java/com/example/wetherappcourse/fragments/MainFragment.kt
@@ -1,14 +1,19 @@
 package com.example.wetherappcourse.fragments
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.wetherappcourse.R
 import com.example.wetherappcourse.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private lateinit var plauncher: ActivityResultLauncher<String>
     private  lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +22,26 @@ class MainFragment : Fragment() {
         binding = FragmentMainBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+
+    }
+
+    private fun permissionListener(){
+        plauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission(){
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION))
+            permissionListener()
+        plauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+
     }
 
     companion object {
